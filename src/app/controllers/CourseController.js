2ffa6c69207aee401f359119a11ff2503e11fc55
KayJuno <dangdan2807@gmail.com>
2021-09-20 22:23:49 +0700

    update: render data

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,31 @@
+const Course = require('../../Models/Course');
+const { mongooseToObject } = require('../../util/mongoose');
+
+class CourseController {
+    // [GET] /course/:slug
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => {
+                res.render('courses/show', { course: mongooseToObject(course) });
+            })
+            .catch(next);
+    }
+
+    // [GET] /course/create
+    create(req, res) {
+        res.render('courses/create');
+    }
+
+    // [POST] /course/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${formData.videoId}/sddefault.jpg`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/'))
+            .catch((error) => {});
+    }
+}
+
+module.exports = new CourseController();
