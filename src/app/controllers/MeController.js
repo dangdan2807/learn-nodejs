b0c0553d7fa4ae9736b8f8ffc7d3854bb95bbeb2
KayJuno <dangdan2807@gmail.com>
2022-01-02 20:20:29 +0700

    add:  Sort Middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,7 +4,16 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/stored/songs
     storedSongs(req, res, next) {
-        Promise.all([Song.find({}), Song.countDocumentsDeleted()])
+        let songQuery = Song.find({});
+
+        // res.json(res.locals._sort);
+        if (req.query.hasOwnProperty('_sort')) {
+            songQuery = songQuery.sort({
+                [req.query.column]: req.query.type
+            });
+        }
+
+        Promise.all([songQuery, Song.countDocumentsDeleted()])
             .then(([songs, deletedCount]) => {
                 res.render('me/stored-songs', {
                     deletedCount,
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+const sortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -19,6 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
+// custom middlewares
+app.use(sortMiddleware);
+
 // HTTP logger
 // app.use(morgan("combined"));
 
@@ -29,6 +34,28 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+                            <span class="${icon}"></span>
+                        </a>`;
+            },
         },
     }),
 );
